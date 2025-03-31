31143cbfd3ee3bd6b8b101a4be4f0bb1916f6936
Shikiso <lewisemerson00@gmail.com>
2025-03-31 13:18:47 +0800

    creating packet

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from scapy.all import sniff, Ether
+from scapy.all import sniff, Ether, send, IP, TCP
 
 interface = "lo"
 count = 1
 
 def handle(packet):
-    print(packet.show())
+    packet[3].load = b"Goodbye"
+    print(packet)
+    send(IP(dst=packet[1].dst)/TCP(dport=packet[2].dport, flags=packet[2].flags)/"Goodbye")
 
 sniff(iface=interface, count=count, prn=handle)
